5cc49248a03db6c6aa1f816ca63999c32fdfb97f
JM17mc <jhnmchlmamac@gmail.com>
2025-04-09 19:26:10 +0800

    Add files via upload

--- try.py
+++ try.py
@@ -2,10 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.title("Widget Ga ni Mac")
+root.title("Widget Daw Ni Mac")
 root.geometry("800x500")
 
-label = tk.Label(root, text="Welcome, Bwisitor!", font=('Arial', 18))
+label = tk.Label(root, text="Welcome, User!", font=('Arial', 18))
 label.pack(padx=20, pady=20)
 
 textbook = tk.Text(root, height=3, font=('Arial',16))
